38607b5dbaebd36b0aef5c30f7a607b15fdf4982
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2023-10-19 08:55:48 +0100

    updated

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -1,72 +1,72 @@
-// 'use client';
-// import React from 'react';
-// import { useCallback } from 'react';
-// import Particles from 'react-particles';
-// import type { Container, Engine } from 'tsparticles-engine';
-// import { loadFull } from 'tsparticles';
-// import particleConfig from '../../particleConfig.json';
-
-// function ParticlesComponent() {
-//   const particlesInit = useCallback(async (engine: Engine) => {
-//     await loadFull(engine);
-//   }, []);
-
-//   const particlesLoaded = useCallback(
-//     async (container: Container | undefined) => {
-//       await console.log(container);
-//     },
-//     []
-//   );
-//   return (
-//     <Particles
-//       id='tsparticles'
-//       options={particleConfig}
-//       init={particlesInit}
-//       loaded={particlesLoaded}
-//       style={{ position: 'absolute', top: 0, left: 0, zIndex: -1 }}
-//     />
-//   );
-// }
-
-// export default ParticlesComponent;
-
+'use client';
 import React from 'react';
+import { useCallback } from 'react';
+import Particles from 'react-particles';
+import type { Container, Engine } from 'tsparticles-engine';
+import { loadFull } from 'tsparticles';
+import particleConfig from '../../particleConfig.json';
 
-function Particles() {
+function ParticlesComponent() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
   return (
-    <div className='min-h-screen fixed pt-20'>
-      <div className='flex items-center justify-center w-full wrap flex-wrap opacity-20'>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-        <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
-      </div>
-    </div>
+    <Particles
+      id='tsparticles'
+      options={particleConfig}
+      init={particlesInit}
+      loaded={particlesLoaded}
+      style={{ position: 'absolute', top: 0, left: 0, zIndex: -1 }}
+    />
   );
 }
 
-export default Particles;
+export default ParticlesComponent;
+
+// import React from 'react';
+
+// function Particles() {
+//   return (
+//     <div className='min-h-screen fixed pt-20'>
+//       <div className='flex items-center justify-center w-full wrap flex-wrap opacity-20'>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri justify-self-end'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//         <div className='circle w-36 h-36 border-2 rounded-full border-pri'></div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Particles;

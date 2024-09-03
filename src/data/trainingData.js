e8d1d539aa34ab1c61405829d7b33685ea9731fb
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2024-09-03 12:15:16 +0100

    added kh devops page

---
+++ src/data/trainingData.js
@@ -0,0 +1,58 @@
+export const trainingData = [
+  {
+    id: 'aws',
+    title: 'AWS Cloud Services',
+    paragraph: `Amazon Web Services provides a comprehensive suite of services for building, deploying, and managing applications. The AWS training is targeted at a demographic consisting mostly of newcomers who want to immediately acquire useful knowledge that will give them entry-level jobs as AWS cloud practitioners. The goal is for participants to acquire the AWS CCP (Certified Cloud Practitioner) certification after completing the course.`,
+    btnColor: '#8FA60C',
+    imageSrc: '/images/aws.svg',
+    details: [
+      {
+        img: '/images/duration-aws.svg',
+        title: 'Duration',
+        items: ['4 weeks'],
+      },
+      {
+        img: '/images/weekly-aws.svg',
+        title: 'Weekly sessions',
+        items: ['2 hours per day', '3 days per week'],
+        listStyle: 'disc',
+      },
+      { img: '/images/mode-aws.svg', title: 'Mode', items: ['Online/webinar'] },
+      {
+        img: '/images/cost-aws.svg',
+        title: 'Cost',
+        items: ['$250 per participant'],
+      },
+    ],
+  },
+  {
+    id: 'azure',
+    title: 'Azure Training',
+    paragraph: `Azure, Microsoft's cloud computing platform, provides a comprehensive suite of services for building, deploying, and managing applications. The Azure training is targeted at a demographic consisting mostly of newcomers to the Cloud DevOps space who want to immediately acquire useful knowledge that places them in a position to get entry-level jobs in DevOps. Ultimately, the goal is for participants to acquire the AZ900 certification after completing the course.`,
+    btnColor: '#335CA6',
+    imageSrc: '/images/azure.svg',
+    details: [
+      {
+        img: '/images/duration-azure.svg',
+        title: 'Duration',
+        items: ['4 weeks'],
+      },
+      {
+        img: '/images/weekly-azure.svg',
+        title: 'Weekly sessions',
+        items: ['2 hours per day', '3 days per week'],
+        listStyle: 'disc',
+      },
+      {
+        img: '/images/mode-azure.svg',
+        title: 'Mode',
+        items: ['Online/webinar'],
+      },
+      {
+        img: '/images/cost-azure.svg',
+        title: 'Cost',
+        items: ['$250 per participant'],
+      },
+    ],
+  },
+];

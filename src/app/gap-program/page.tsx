a77c57bb2f9b9449ab6868539001e6074d011d67
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2024-10-25 00:12:04 +0100

    feat: added gap-program page

--- src/app/gap-program/page.tsx
+++ src/app/gap-program/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function page() {
   return (
     <div className="min-h-screen bg-white flex justify-center items-center">
-      <h1 className="font-bold text-4xl">GAP PROGRAM</h1>
+      <h1 className="font-bold text-4xl text-black">GAP PROGRAM</h1>
     </div>
   );
 }
